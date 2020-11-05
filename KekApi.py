6d5240d9faeb1649978fe6f9f203e8d566dfb875
Nikita <nnikonov2006@gmail.com>
2020-11-05 16:24:33 +0300

    KekApi rewrite to get news

--- KekApi.py
+++ KekApi.py
@@ -1,39 +1,44 @@
 import re
 import requests as r
 
-# давай-ка кекнем все эти статьи!
+# getting all news inarray
 
+def news():
 
-def article(id):
     def cleanhtml(raw_html):
         cleanr = re.compile('<.*?>')
         cleantext = re.sub(cleanr, '', raw_html)
         return cleantext
 
-    def tags(value):
-        array = []
-        for tag in value:
-            array.append(tag['titleHtml'])
-        return array
+    def get_page(num):
+        API_URL = 'https://m.habr.com/kek/v2/articles?fl=ru&hl=ru&news=true&page=%d' % (num)
+        response = r.get(API_URL)
+        if response.status_code == 200:
+            json = response.json()
+            return json
 
-    API_URL = 'https://m.habr.com/kek/v2/articles/%d/?fl=ru&hl=ru' % (id)
-    article = {}
+    pages = get_page(1)['pagesCount']
 
-    response = r.get(API_URL)
+    news = []
 
-    if response.status_code == 200:
-        json = response.json()
+    count = 0
+    for i in range(1, pages + 1):
+        page = get_page(i)
+        for ref in page['articleRefs'].values():
+            piece_of_news = {}
 
-        article['status'] = 'ok'
-        article['title'] = cleanhtml(json['titleHtml'])
-        article['text'] = cleanhtml(json['textHtml'])
-        article['tags'] = tags(json['tags'])
-        article['id'] = json['id']
-        article['score'] = json['statistics']['score']
-        article['votes'] = json['statistics']['votesCount']
-        article['comments'] = json['statistics']['commentsCount']
-        article['favorites'] = json['statistics']['favoritesCount']
-    else:
-        article['status'] = 'err'
+            piece_of_news['views'] = ref['statistics']['readingCount']
+            piece_of_news['rating'] = ref['statistics']['score']
+            piece_of_news['votes'] = ref['statistics']['votesCount']
+            piece_of_news['favorites'] = ref['statistics']['favoritesCount']
+            piece_of_news['comments'] = ref['statistics']['commentsCount']
+            piece_of_news['pubdate'] = ref['timePublished']
+            piece_of_news['text'] = cleanhtml(ref['leadData']['textHtml'])
 
-    return article
+            news.append(piece_of_news)
+            count = count + 1
+            print(count)
+
+    print('Data received.')
+
+    return news
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import KekApi as kek
+import json
+
+# pushing all news in data.json
 
 if __name__ == "__main__":
+    news = kek.news()
 
-    a = kek.article(515636)
-    
-    if a['status'] == 'ok':
-        print(a)
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(news, f, ensure_ascii=False, indent=4)
+
+    print('Completed. Look for data.json file.')
